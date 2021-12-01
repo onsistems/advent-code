279f070f15cc098051d3e4e546480eaa6aaa97d6
Ovidiu Muresan <47462138+ONsistems@users.noreply.github.com>
2021-12-01 08:01:34 +0100

    Day 1 Part 2

--- 2021/1/index.php
+++ 2021/1/index.php
@@ -5,14 +5,26 @@ $data = file_get_contents('input.txt');
 function inter($n) { return (int)$n; }
 $arr_number = array_map("inter", preg_split('/\n+/', $data, -1, PREG_SPLIT_NO_EMPTY));
 
-var_dump($arr_number);
-$mayor = 0;
-foreach ($arr_number as $key => $value) {
-	if($key > 0) {
-		if($value > $arr_number[$key-1]){
-			$mayor++;
+echo countMayor($arr_number), PHP_EOL; //1195
+
+function countMayor(array $data):int {
+	$mayor = 0;
+	foreach ($data as $key => $value) {
+		if($key > 0) {
+			if($value > $data[$key-1]){
+				$mayor++;
+			}
 		}
+	}
+	return $mayor;
+}
+
+$threesome = [];
+
+foreach ($arr_number as $key => $value) {
+	if(isset($arr_number[$key+1]) && isset($arr_number[$key+2])) {
+		array_push($threesome, $value+$arr_number[$key+1]+$arr_number[$key+2]);
 	}
 }
 
-echo $mayor;
+echo countMayor($threesome);//1235
